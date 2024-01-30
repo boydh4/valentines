d31ad71b71cee713a34baa8e201adf1bcbf7b062
boydh4 <boydh4@mcmaster.ca>
2024-01-30 00:05:51 -0500

    Finished no pic

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let noButtonMoveCount = 0;
+
 document.getElementById('nextButton').addEventListener('click', function() {
     document.getElementById('message').style.display = 'none';
     document.getElementById('question').style.display = 'block';
@@ -5,12 +7,28 @@ document.getElementById('nextButton').addEventListener('click', function() {
 
 document.getElementById('yesButton').addEventListener('click', function() {
     document.getElementById('question').style.display = 'none';
-    document.getElementById('response').innerHTML = '<h1>Ok my valentine, dinner and drinks on me for the 14th. I love you so much</h1>';
+    document.getElementById('response').innerHTML = '<h1>Ok my valentine, I\'ll see you on the 14th for our mystery date.</h1>';
     document.getElementById('response').style.display = 'block';
 });
 
-document.getElementById('noButton').addEventListener('mouseover', function() {
-    this.style.position = 'absolute';
-    this.style.top = Math.random() * window.innerHeight + 'px';
-    this.style.left = Math.random() * window.innerWidth + 'px';
+const noButton = document.getElementById('noButton');
+noButton.addEventListener('mouseover', function() {
+    if (noButtonMoveCount < 5) {
+        this.style.position = 'absolute';
+        this.style.top = Math.random() * window.innerHeight + 'px';
+        this.style.left = Math.random() * window.innerWidth + 'px';
+        noButtonMoveCount++;
+    }
 });
+
+noButton.addEventListener('click', function() {
+    if (noButtonMoveCount >= 5) {
+        document.getElementById('question').style.display = 'none';
+        document.getElementById('wrong-answer').style.display = 'block';
+    }
+});
+
+document.getElementById('goBackButton').addEventListener('click', function() {
+    document.getElementById('wrong-answer').style.display = 'none';
+    document.getElementById('question').style.display = 'block';
+    noButtonMoveCount
