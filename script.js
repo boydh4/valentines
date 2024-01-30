d3b5be7d55bc572941cb596f7ba0f6e9cd0ff3e9
boydh4 <boydh4@mcmaster.ca>
2024-01-30 00:23:23 -0500

    No pic

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById('nextButton').addEventListener('click', function() {
 
 document.getElementById('yesButton').addEventListener('click', function() {
     document.getElementById('question').style.display = 'none';
-    document.getElementById('response').innerHTML = '<h1>Ok my valentine, I\'ll see you on the 14th for our mystery date.</h1>';
+    document.getElementById('response').innerHTML = '<h1>Ok then my valentine, I\'ll see your beautiful face across the dinner table February 14th at 7:00 pm. I love you.</h1>';
     document.getElementById('response').style.display = 'block';
 });
 
@@ -30,5 +30,4 @@ noButton.addEventListener('click', function() {
 
 document.getElementById('goBackButton').addEventListener('click', function() {
     document.getElementById('wrong-answer').style.display = 'none';
-    document.getElementById('question').style.display = 'block';
-    noButtonMoveCount
+    document.getElementById('question').style
