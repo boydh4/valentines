ec5c27e27bef1e33ed09269b3fbfd5ad062fbe7d
boydh4 <boydh4@mcmaster.ca>
2024-01-29 23:55:38 -0500

    Finished

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById('nextButton').addEventListener('click', function() {
 
 document.getElementById('yesButton').addEventListener('click', function() {
     document.getElementById('question').style.display = 'none';
-    document.getElementById('response').innerHTML = '<h1>Ok my valentine, I\'ll see you on the 14th for our mystery date.</h1>';
+    document.getElementById('response').innerHTML = '<h1>Ok my valentine, dinner and drinks on me for the 14th. I love you so much</h1>';
     document.getElementById('response').style.display = 'block';
 });
 
